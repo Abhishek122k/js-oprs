85ad677d02b2b3d04a4476b16616ca030ccc16b1
AbhishekNaik1112 <abishek.naik@kalvium.community>
2023-09-12 17:53:08 +0530

    fc

---
+++ script.js
@@ -0,0 +1,126 @@
+var x = 5;
+var y = 2;
+
+// Challenge 1: Perform the addition operation (add x and y) and print the answer in the console.
+console.log(x+y);
+// Challenge 2: Perform the Subtraction operation (subtract y from x) and print the answer in the console.
+console.log(x-y);
+// Challenge 3: Perform Multiplication operation (multiply x from y) and print the answer in console.
+console.log(x*y);
+// Challenge 4: Perform Division operation (Divide x by y) and print the answer in console.
+console.log(x/y);
+// Challenge 5: perform division operation (divide x by y) and print the remainder in console.
+console.log(x%y);
+// Challenge 6: Perform Exponent operation and print the answer in console.
+console.log(x**y);
+
+var z = 20;
+// Challenge 7: calculate "x" to the power of the "y" and print the answer in console.
+console.log(x**y);
+// Challenge 8: Decrement the value of the variable "z" by 1 and print the z value in the console.
+z--;
+console.log(z);
+var a = 20;
+
+var b = 10;
+
+var c = 15;
+
+var d = 20;
+
+var tempa=a;
+var tempb=b;
+var tempc=c;
+var tempd=d;
+
+// Challenge 9: Perform Addition assignment operation to the variable a;
+a+=a;
+console.log(a);
+
+// Challenge 10: Perform 	Subtraction assignment operation to the variable b;
+b-=b
+console.log(b);
+
+// Challenge 11: Perform 	Multiplication assignment operation to the variable c;
+c*=c;
+console.log(c);
+
+// Challenge 12: Perform 	Division assignment operation to the variable c;
+d/=d;
+console.log(d);
+
+// Challenge 13: Perform 2 Strict equality Comparison operations, in which one should return true and one should return false.
+if (tempa===tempd) {
+    console.log("true"); 
+  } else {
+    console.log("false");
+  }
+
+  if (tempb===tempc) {
+    console.log("true"); 
+  } else {
+    console.log("false");
+  }
+
+// Challenge 14: Perform 2 Strict-non-equality Comparison operations, in which one should return true and one should return false.
+if (tempa==tempd) {
+    console.log("true"); 
+  } else {
+    console.log("false");
+  }
+
+  if (tempb==tempc) {
+    console.log("true"); 
+  } else {
+    console.log("false");
+  }
+
+// Challenge 15: Perform 2 Less than Comparison operations, in which one should return true and one should return false.
+if (6 < 11) {
+  console.log("true"); 
+} else {
+  console.log("false");
+}
+
+if (11 < 6) {
+  console.log("true"); 
+} else {
+  console.log("false");
+}
+
+// Challenge 16: Perform 2 Greater than Comparison operations, in which one should return true and one should return false.
+if (15 > 11) {
+  console.log("true"); 
+} else {
+  console.log("false");
+}
+
+if (2 > 5) {
+  console.log("true"); 
+} else {
+  console.log("false");
+}
+// Challenge 17: Perform 2 Less than or equal to Comparison operations, in which one should return true and one should return false.
+if (4<=10) {
+  console.log("true"); 
+} else {
+  console.log("false");
+}
+
+if (60<=100) {
+  console.log("true"); 
+} else {
+  console.log("false");
+}
+// Challenge 18: Perform 2 Greater than or equal to Comparison operations, in which one should return true and one should return false.
+if (41>=32) {
+  console.log("true"); 
+} else {
+  console.log("false");
+}
+
+if (20>=40) {
+  console.log("true"); 
+} else {
+  console.log("false");
+}
